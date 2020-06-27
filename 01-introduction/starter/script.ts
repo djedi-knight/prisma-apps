661f4de881285a55d73d6e3d8860bf6346408053
Shawn Daichendt <shawn.daichendt@gmail.com>
2020-06-27 11:35:40 -0400

    Included posts in user API call

--- 01-introduction/starter/script.ts
+++ 01-introduction/starter/script.ts
@@ -4,10 +4,12 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
-  // ... you will write your Prisma Client queries here
-  // Get all users
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
+  // Get all users (include posts)
+  const allUsers = await prisma.user.findMany({
+    include: { posts: true },
+  })
+  // use `console.dir` to print nested objects
+  console.dir(allUsers, { depth: null })
 }
 
 main()
