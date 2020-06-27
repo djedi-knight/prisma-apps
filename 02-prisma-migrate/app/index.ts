6c7a136914b3a7265aa523a5c731113aabaf9ebe
Shawn Daichendt <shawn.daichendt@gmail.com>
2020-06-27 16:44:22 -0400

    Commented out create call

--- 02-prisma-migrate/app/index.ts
+++ 02-prisma-migrate/app/index.ts
@@ -4,25 +4,36 @@ const prisma = new PrismaClient()
 
 async function main() {
   // Create a user
-  await prisma.user.create({
-    data: {
-      name: 'Alice',
-      email: 'alice@prisma.io',
-      posts: {
-        create: { title: 'Hello World' },
-      },
-      profile: {
-        create: { bio: 'I like turtles' },
-      },
-    },
+  // Note: this should throw a
+  // 'Unique constraint failed on the fields: (`email`)' Error 
+  // if run more than once (and that's good!)
+  // await prisma.user.create({
+  //   data: {
+  //     name: 'Alice',
+  //     email: 'alice@prisma.io',
+  //     posts: {
+  //       create: { title: 'Hello World' },
+  //     },
+  //     profile: {
+  //       create: { bio: 'I like turtles' },
+  //     },
+  //   },
+  // })
+
+  // Update the user
+  await prisma.post.update({
+    where: { id: 1 },
+    data: { published: true },
   })
-  // Get all users
+  
+  // Get all users (and their posts/ profiles)
   const allUsers = await prisma.user.findMany({
     include: {
       posts: true,
       profile: true,
     },
   })
+  console.log('all users:')
   console.dir(allUsers, { depth: null })
 }
 
