d4f10f16e7db5b952d4a69c43f65c6335422cbe3
Shawn Daichendt <shawn.daichendt@gmail.com>
2020-06-27 16:31:44 -0400

    Added a query to create a user

--- 02-prisma-migrate/app/index.ts
+++ 02-prisma-migrate/app/index.ts
@@ -3,9 +3,27 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main() {
+  // Create a user
+  await prisma.user.create({
+    data: {
+      name: 'Alice',
+      email: 'alice@prisma.io',
+      posts: {
+        create: { title: 'Hello World' },
+      },
+      profile: {
+        create: { bio: 'I like turtles' },
+      },
+    },
+  })
   // Get all users
-  const allUsers = await prisma.user.findMany()
-  console.log(allUsers)
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true,
+    },
+  })
+  console.dir(allUsers, { depth: null })
 }
 
 main()
