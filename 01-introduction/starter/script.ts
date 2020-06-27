9b19dd4b77920d4a35b8b0c87f99ef26f04fdb06
Shawn Daichendt <shawn.daichendt@gmail.com>
2020-06-27 11:55:17 -0400

    Added update post call to publish the post

--- 01-introduction/starter/script.ts
+++ 01-introduction/starter/script.ts
@@ -5,13 +5,18 @@ const prisma = new PrismaClient()
 // A `main` function so that you can use async/await
 async function main() {
   // Create a post
-  const post = await prisma.post.create({
+  await prisma.post.create({
     data: {
       title: "Prisma makes databases easy",
       author: {
         connect: { email: "sarah@prisma.io" },
       },
     },
+  })
+  // Publish the post
+  const post = await prisma.post.update({
+    where: { id: 2 },
+    data: { published: true },
   })
   console.log(post)
   // Get all users (include posts)
