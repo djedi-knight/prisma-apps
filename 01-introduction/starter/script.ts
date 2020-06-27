e88963340cd4876e37af8ae0cc3869d7ab62b3bd
Shawn Daichendt <shawn.daichendt@gmail.com>
2020-06-27 11:41:35 -0400

    Added a post create call

--- 01-introduction/starter/script.ts
+++ 01-introduction/starter/script.ts
@@ -4,6 +4,16 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
+  // Create a post
+  const post = await prisma.post.create({
+    data: {
+      title: "Prisma makes databases easy",
+      author: {
+        connect: { email: "sarah@prisma.io" },
+      },
+    },
+  })
+  console.log(post)
   // Get all users (include posts)
   const allUsers = await prisma.user.findMany({
     include: { posts: true },
